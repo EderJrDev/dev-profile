2f1473f881e111b57a324127d1c8bcbc9a169a8b
Eder Jr <ederjuninho2003@gmail.com>
2024-01-29 17:59:08 -0300

    feat: add new pdf in profile

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -1,3 +1,5 @@
+"use client";
+import Link from "next/link";
 import Image from "next/image";
 
 import { Linkedin, Github } from "lucide-react";
@@ -5,6 +7,16 @@ import { Linkedin, Github } from "lucide-react";
 import developer from "../../public/profile.png";
 
 const Hero = () => {
+  const enviarEmail = () => {
+    window.location.href = "mailto:ederjuninho2003@gmail.com";
+  };
+
+  const baixarCV = () => {
+    // Lógica para baixar o arquivo PDF (substitua 'caminho/do/arquivo.pdf' pelo caminho real do seu arquivo)
+    const urlDoArquivoPDF = "../../public/Programador.pdf";
+    window.open(urlDoArquivoPDF, "_blank");
+  };
+
   return (
     <section className="h-screen flex flex-col justify-center items-center">
       <div className="flex flex-col lg:flex-row justify-center items-center rounded-full my-2 mx-auto">
@@ -25,20 +37,34 @@ const Hero = () => {
             Desenvolvedor Frontend
           </p>
           <div className="flex">
-            <button className=" p-3 me-3 rounded-full border border-black bg-white hover:bg-black hover:text-white dark:text-black dark:hover:bg-black dark:hover:text-white font-bold flex-1">
-              Download CV
-            </button>
-            <button className="p-3 me-3 rounded-full border text-white border-black bg-black hover:bg-white hover:text-black font-bold flex-1">
+            <Link href="/caminho/do/arquivo.pdf" passHref legacyBehavior>
+              <div className="p-3 me-3 rounded-full border border-black bg-white hover:bg-black hover:text-white dark:text-black dark:hover:bg-black dark:hover:text-white font-bold flex-1">
+                Download CV
+              </div>
+            </Link>
+            <button
+              onClick={enviarEmail}
+              className="p-3 me-3 rounded-full border text-white border-black bg-black hover:bg-white hover:text-black font-bold flex-1"
+            >
               Contato
             </button>
           </div>
 
           <div className="flex pt-3 text-center gap-5 justify-center">
             <div className="p-3 rounded-full border border-black hover:bg-black hover:text-white text-center justify-center">
-              <Linkedin className=" m-auto fill-white" />
+              <Link
+                href="https://www.linkedin.com/in/ederjuniordev/"
+                target="_blank"
+              >
+                {" "}
+                <Linkedin className=" m-auto fill-white" />
+              </Link>
             </div>
             <div className="p-3 rounded-full border border-black text-center justify-center  hover:bg-black hover:text-white ">
-              <Github className=" m-auto" />
+              <Link href="  https://github.com/EderJrDev" target="_blank">
+                {" "}
+                <Github className=" m-auto" />
+              </Link>
             </div>
           </div>
         </div>
